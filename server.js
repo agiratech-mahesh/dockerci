5c49f021e2782fc7b20fdbb12af7771b914288e8
mahesh <102363912+agiratech-mahesh@users.noreply.github.com>
2023-04-12 11:06:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const ronin = require('ronin-server')
-const mocks = require('ronin-mocks')
-const database = require('ronin-database')
+const ronin = require('ronin-server');
+const mocks = require('ronin-mocks');
+const database = require('ronin-database');
 const server = ronin.server()
 
 database.connect( process.env.CONNECTIONSTRING )
